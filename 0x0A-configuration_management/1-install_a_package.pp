1474f79e384b8fbad0059a73ef001e7031ab0fea
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-19 08:41:35 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,20 @@
-# Using Puppet, install flask from pip3
+# Install python3-pip
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1 using pip3
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+  require => Package['python3-pip'],
 }
